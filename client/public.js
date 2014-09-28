9f9537d9e2f6110872bafebfb72e4c013f61df84
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-09-28 15:27:49 +0300

    no background moving

--- client/public.js
+++ client/public.js
@@ -4,3 +4,12 @@ Template.public.helpers({
 			return marked(Collection.findOne('text').text)
 	}
 })
+
+Template.public.rendered = function() {
+	var width = $(document).width()
+	var height = $(document).height() + 500
+
+	var t = new Trianglify({noiseIntensity: 0});
+	var pattern = t.generate(width, height);
+	$('body').css('background-image', pattern.dataUrl)
+}
